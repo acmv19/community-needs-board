5dae82160bae0ac9ca5853cbc1d9282b472b9c07
anamaria <anamariamaldonado1994@gmail.com>
2025-08-11 23:17:26 -0400

    Fix lint issues: double quotes and trailing whitespace

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
 
      has_many :posts
      has_many :comments
-     
+
      validates :name, presence: true
      validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
      validates :password, presence: true
